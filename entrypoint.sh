faa76458ffcef3db460ea5772718e25034b1d703
Kyle D. Weber <54750041+kdw2126@users.noreply.github.com>
2022-10-11 19:44:15 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ git fetch --prune --unshallow --no-tags
 
 SQL_FILE_PATTERN="${FILE_PATTERN:?}"
 SOURCE_REFERENCE="origin/${GITHUB_PULL_REQUEST_BASE_REF:?}"
-changed_files=$(git diff --relative=real-markets --name-only --no-color "$SOURCE_REFERENCE" "HEAD" -- "${SQLFLUFF_PATHS:?}" |
+changed_files=$(git diff --name-only --no-color "$SOURCE_REFERENCE" "HEAD" -- "${SQLFLUFF_PATHS:?}" |
   grep -e "${SQL_FILE_PATTERN:?}" |
   xargs -I% bash -c 'if [[ -f "%" ]] ; then echo "%"; fi' || :)
 echo "Changed files:"
